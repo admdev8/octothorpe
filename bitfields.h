6c14e845a50bb83a5f87760bf6206324359c0e97
Dennis Yurichev <dennis@yurichev.com>
2013-04-06 18:48:39 +0300

    =TRUE/FALSE booleans should be used instead of C++ ones

--- bitfields.h
+++ bitfields.h
@@ -1,5 +1,5 @@
 #pragma once
 
-#define IS_SET(flag, bit)       (((flag) & (bit)) ? true : false)
+#define IS_SET(flag, bit)       (((flag) & (bit)) ? TRUE : FALSE)
 #define SET_BIT(var, bit)       ((var) |= (bit))
 #define REMOVE_BIT(var, bit)    ((var) &= ~(bit))
