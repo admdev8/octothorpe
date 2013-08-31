d19a198ca7fddf2142ed7c35512006a64fa70b9a
Dennis Yurichev <dennis@yurichev.com>
2013-08-31 07:49:28 +0300

    my own oassert() with hookers and blackjack

---
+++ oassert.c
@@ -0,0 +1,29 @@
+/*
+ *             _        _   _                           
+ *            | |      | | | |                          
+ *   ___   ___| |_ ___ | |_| |__   ___  _ __ _ __   ___ 
+ *  / _ \ / __| __/ _ \| __| '_ \ / _ \| '__| '_ \ / _ \
+ * | (_) | (__| || (_) | |_| | | | (_) | |  | |_) |  __/
+ *  \___/ \___|\__\___/ \__|_| |_|\___/|_|  | .__/ \___|
+ *                                          | |         
+ *                                          |_|
+ *
+ * Written by Dennis Yurichev <dennis(a)yurichev.com>, 2013
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "oassert.h"
+
+_Noreturn void _oassert (const char *msg, const char *file, unsigned line, const char *func)
+{
+	fprintf (stderr, "Assertion failed: %s\n", msg);
+	fprintf (stderr, "File/line: %s:%d, function: %s\n", file, line, func);
+	fprintf (stderr, "Exiting.\n");
+	exit(0);
+};
+
